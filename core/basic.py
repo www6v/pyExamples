01890880a1725da8cd240b15e30979bab31e1d77
wangwei <www6v@126.com>
2022-12-14 15:49:34 +0800

    init

--- core/basic.py
+++ core/basic.py
@@ -1,25 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+##
 def all_unique(lst):
         return len(lst) == len(set(lst))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def listFunction():
     x = [1, 1, 2, 2, 3, 2, 3, 4, 5, 6]
     y = [1, 2, 3, 4, 5]
     print(all_unique(x))
     print(all_unique(y))
 
 
-
-
-
+    l = [9, 8, 6, 5]
+    print(l[1:2]);
---
+++ core/main.py
@@ -0,0 +1,14 @@
+import basic
+
+if __name__ == '__main__':
+    basic.print_hi('PyCharm')
+
+    basic.listFunction()
+
+
+
+
+
+
+
+
