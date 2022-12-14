46ba0447b6f66089274743bb3eb6eeaf24b14342
wangwei <www6v@126.com>
2022-12-14 16:38:35 +0800

    add

--- core/basic.py
+++ core/basic.py
@@ -1,17 +1,38 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-##
 def all_unique(lst):
         return len(lst) == len(set(lst))
 
-def listFunction():
+def listAndTuple():
+    ##
     x = [1, 1, 2, 2, 3, 2, 3, 4, 5, 6]
     y = [1, 2, 3, 4, 5]
     print(all_unique(x))
     print(all_unique(y))
 
+    ## list vs. tup
+    l = [3, 2, 3, 7, 8, 1]
+    print(l[1:2])
+    l.count(3)
+    l.index(7)
+    l.reverse()
+    l.sort()
 
-    l = [9, 8, 6, 5]
-    print(l[1:2]);
+    tup = (3, 2, 3, 7, 8, 1)
+    tup.count(3)
+    tup.index(7)
+    list(reversed(tup))
+    sorted(tup)
+
+
+def dictAndSet():
+    d = {'name': 'jason', 'age': 20}
+
+    d['gender'] = 'male'
+    d['dob'] = '1999-02-01'
+    d['dob'] = '1998-01-01'
+    d.pop('dob')
+    print(d)
+
+    s = {1, 2, 3}
+    s.add(4)
+    s.remove(4)
+    print(s)
--- core/main.py
+++ core/main.py
@@ -1,9 +1,8 @@
 import basic
 
 if __name__ == '__main__':
-    basic.print_hi('PyCharm')
-
-    basic.listFunction()
+    basic.listAndTuple()
+    basic.dictAndSet();
 
 
 
