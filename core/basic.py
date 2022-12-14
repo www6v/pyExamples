359581382b3355f410ed8b8a49d4ab42f1b7ead8
wangwei <www6v@126.com>
2022-12-14 17:16:56 +0800

    add

--- core/basic.py
+++ core/basic.py
@@ -1,3 +1,9 @@
+class MyInputError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return ("{} is invalid input".format(repr(self.value)))
+
 def all_unique(lst):
         return len(lst) == len(set(lst))
 
@@ -73,4 +79,22 @@ def ifAndLoop():
       if index < 5:
          print(index,item)
 
+def errerHandling():
+    try:
+        s = input('please enter two numbers separated by comma: ')
+        num1 = int(s.split(',')[0].strip())
+        num2 = int(s.split(',')[1].strip())
+    except ValueError as err:
+        print('Value Error: {}'.format(err))
+    except IndexError as err:
+        print('Index Error: {}'.format(err))
+    except:
+        print('Other error')
+    print('continue')
 
+
+def errorHandling1():
+    try:
+        raise MyInputError(1)
+    except MyInputError as err:
+        print('error: {}'.format(err))
--- core/main.py
+++ core/main.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
     basic.listAndTuple()
     basic.dictAndSet()
     basic.ifAndLoop()
+    # basic.errerHandling()
+    basic.errorHandling1()
 
 
 
