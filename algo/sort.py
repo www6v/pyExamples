7d48e6d24aaf20283ce845f9ddcd1df675519e81
www6v <www6v@126.com>
2025-03-31 15:36:00 +0800

    f

---
+++ algo/sort.py
@@ -0,0 +1,26 @@
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot] 
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+if __name__ == "__main__" :
+    arr = [4, 5, 1, 2, 3]
+    print(bubble_sort(arr))
+
+    arr = [3, 4, 1, 2, 5]
+    print(quick_sort(arr))
+
